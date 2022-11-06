231c7af5bc60f80ee90db8b8ff05d2563ff9f650
adi <116135080+adityakum22@users.noreply.github.com>
2022-11-07 01:27:51 +0530

    file name

---
+++ f.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   /* my first program in C */
+   printf("Hello, World! \n");
+    printf("Hello, World! \n");
+     printf("Hello, World! \n");
+      printf("Hello, World! \n");
+       printf("Hello, World! \n");
+        printf("Hello, World! \n");
+         printf("Hello, World! \n");
+         
+   return 0;
+}
